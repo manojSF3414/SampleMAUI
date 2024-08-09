69e366525202a519f890a63ed80eeb9d28889143
manojSF3414 <manojkumar.a@syncfusion.com>
2024-08-09 18:19:26 +0530

    New Sample

---
+++ CustomisedEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMAUI
+{
+    internal class CustomisedEntry:Entry
+    {
+        internal CustomisedEntry()
+        {
+            IsSpellCheckEnabled = false;
+            IsTextPredictionEnabled = false;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SampleMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int PageNumber = 0;
+        Entry pageEntry;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            pageEntry = new Entry
+            {
+                WidthRequest = 58,
+                FontSize = 16,
+                FontAutoScalingEnabled = false,
+                MaxLength = 4,
+                Keyboard = Keyboard.Numeric,
+                HorizontalTextAlignment = TextAlignment.Center,
+            };
+            pageEntry.BindingContext = this;
+            pageEntry.SetBinding(Entry.TextProperty, "PageNumber");
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
